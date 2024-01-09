16a58113eb207e168c31c640708507a9fa8a1dcd
Aldrin Villalobos <alcv@newmediastaff.com>
2024-01-09 14:30:02 +0800

    Added prop-types

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,45 @@
-import React from 'react';
-import { Button } from '@mui/material';
+import React from "react";
+import { Button } from "@mui/material";
+import PropTypes from "prop-types";
 
-export const NMSButton = () => {
+export const NMSButton = ({
+  children,
+  classes,
+  color,
+  component,
+  disabled,
+  disableElevation,
+  disableFocusRipple,
+  disableRipple,
+  fullWidth,
+  href,
+  size,
+  startIcon,
+  endIcon,
+  sx,
+  variant,
+}) => {
   return (
-    <Button>luh</Button>
-  )
+    <Button
+      classes={classes}
+      color={color}
+      component={component}
+      endIcon={endIcon}
+      href={href}
+      size={size}
+      startIcon={startIcon}
+      sx={sx}
+      variant={variant}
+    >
+      {children}
+    </Button>
+  );
+};
+
+NMSButton.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+NMSButton.defaultProps = {
+  children: 'NMS Button'
 }
